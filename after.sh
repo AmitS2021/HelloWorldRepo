dc2bc3fcf3bffed489005a5d107b9a42e32ff55c
AmitS2021 <96290199+AmitS2021@users.noreply.github.com>
2022-02-25 18:38:25 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,23 +1,4 @@
 echo "Deploying to $DEPLOYMENT_GROUP_NAME environment..."
 echo "User is: $USER" 
 
-echo "cd to path..."
-cd /var/www/html/telmyportal/
-
-echo "Clearing local changes..."
-git checkout .
-
-sudo mv ../vendor .
-sudo mv ../uploads frontend/web
-
-echo "Doing environment init ($DEPLOYMENT_GROUP_NAME)..."
-sudo ./init --env=$DEPLOYMENT_GROUP_NAME --overwrite=a
-
-echo "Doing migrate..."
-echo 'yes' | ./yii migrate
-
-echo "Doing composer update..."
-./composer.phar update --no-interaction
-
 echo "Done!"
-
